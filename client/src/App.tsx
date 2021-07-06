d8ee5057a7b8ebff82871da70e19cdef8a1e6b70
Shinji Hirota <shinjishinji1129@gmail.com>
2021-07-06 17:45:22 +0800

    added header

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,23 +1,25 @@
-import React from 'react';
-import './App.css';
-import { Route, Switch } from 'react-router-dom';
-import LandingPage from './pages/LandingPage';
-import AlertDataPage from './pages/AlertDataPage';
-import NavBar from './components/NavBar';
-import { ConnectedRouter } from 'connected-react-router';
-import { history } from './redux/store';
+import React from "react";
+import "./App.css";
+import { Route, Switch } from "react-router-dom";
+import LandingPage from "./pages/LandingPage";
+import AlertDataPage from "./pages/AlertDataPage";
+import NavBar from "./components/NavBar";
+import { ConnectedRouter } from "connected-react-router";
+import { history } from "./redux/store";
 
 function App() {
   return (
     <div className="App">
       <ConnectedRouter history={history}>
-        <div className="fixOnPage"><NavBar /></div>
+        <div className="fixOnPage">
+          <NavBar />
+        </div>
         <Switch>
-          <Route path="/" exact={true} component={LandingPage} />
-          <Route path="/alertData" exact={true} component={AlertDataPage} />
+          {/* <Route path="/" exact={true} component={LandingPage} /> */}
+          {/* <Route path="/alertData" exact={true} component={AlertDataPage} /> */}
+          <Route path="/" exact={true} component={AlertDataPage} />
         </Switch>
       </ConnectedRouter>
-      
     </div>
   );
 }
--- client/src/components/NavBar.tsx
+++ client/src/components/NavBar.tsx
@@ -1,43 +1,156 @@
-import React, { useState } from 'react';
-import { Collapse, Navbar, NavbarToggler, NavbarBrand, Nav, NavItem, NavLink } from 'reactstrap';
-import { push } from 'connected-react-router';
-import { useDispatch } from 'react-redux';
+import React, { useState } from "react";
+import {
+  Collapse,
+  Navbar,
+  NavbarToggler,
+  NavbarBrand,
+  Nav,
+  NavItem,
+  NavLink,
+} from "reactstrap";
+import { push } from "connected-react-router";
+import { useDispatch } from "react-redux";
+import { CompanyName } from "./CompanyName";
+import { CaretIcon, GraphViewIcon, SearchIcon } from "./IconsOnly";
 
 function NavBar() {
-    const [collapsed, setCollapsed] = useState(true);
-    const dispatch = useDispatch();
+  const [collapsed, setCollapsed] = useState(true);
+  const dispatch = useDispatch();
 
-    const toggleNavbar = () => setCollapsed(!collapsed);
-    
-    const HomeBand = () =>{
-        dispatch(push('/'));
-    }
-    const HomeClick = () =>{
-        toggleNavbar();
-        dispatch(push('/'));
-    }
-    const AlertDataPage = () =>{
-        toggleNavbar();
-        dispatch(push('/alertData'));
-    }
-    return (
-        <div>
-            <Navbar color="faded" light>
-                <NavbarBrand onClick={HomeBand} className="mr-auto"><label>Handbrake System</label></NavbarBrand>
-                <NavbarToggler onClick={toggleNavbar} className="mr-2" />
-                <Collapse isOpen={!collapsed} navbar>
-                <Nav navbar>
-                    <NavItem>
-                        <NavLink onClick={HomeClick}><label>Home page</label></NavLink>
-                    </NavItem>
-                    <NavItem>
-                        <NavLink onClick={AlertDataPage}><label>Alert page</label></NavLink>
-                    </NavItem>
-                </Nav>
-                </Collapse>
-            </Navbar>
+  const toggleNavbar = () => setCollapsed(!collapsed);
+
+  const [isOpen, setIsOpen] = useState(false);
+  const [placeHolderText, setPlaceHolderText] = useState("Select");
+  const dropDownList = [
+    "Device ID",
+    "Car Plate",
+    "User",
+    "Phone number",
+    "Location",
+    "Date",
+  ];
+
+  const HomeBand = () => {
+    dispatch(push("/"));
+  };
+  const HomeClick = () => {
+    toggleNavbar();
+    dispatch(push("/"));
+  };
+  const AlertDataPage = () => {
+    toggleNavbar();
+    dispatch(push("/alertData"));
+  };
+  return (
+    <div className="topNavStyle">
+      <div
+        style={{
+          display: "flex",
+          width: "100%",
+          justifyContent: "space-between",
+          alignItems: "center",
+          margin: "8px",
+        }}
+      >
+        <CompanyName />
+        <div className="flex-center" style={{ position: "relative" }}>
+          <div style={{ padding: "8px" }}>Search by:</div>
+          <div
+            style={{
+              color: placeHolderText === "Select" ? "#ccc" : "#555",
+              minWidth: "64px",
+              transition: "all 1s ease",
+            }}
+          >
+            {placeHolderText}
+          </div>
+          <div
+            style={{ padding: "8px", cursor: "pointer" }}
+            onClick={() => setIsOpen(!isOpen)}
+          >
+            <CaretIcon />
+          </div>
+          <div className="flex-center" style={{ padding: "8px" }}>
+            <input
+              className="searchInput"
+              placeholder={"Search"}
+              style={{
+                width: placeHolderText !== "Select" ? "240px" : "0px",
+              }}
+            />
+            <div style={{ cursor: "pointer", padding: "8px" }}>
+              <SearchIcon />
+            </div>
+          </div>
+          <div
+            className="dropDownListContainer"
+            style={{
+              maxHeight: isOpen ? `${(dropDownList.length + 1) * 40}px` : 0,
+            }}
+          >
+            {isOpen &&
+              dropDownList.map((item, idx) => {
+                return (
+                  <div
+                    key={idx}
+                    className="dropDownItem"
+                    style={{ height: isOpen ? "48px" : "0px" }}
+                    onClick={() => {
+                      setPlaceHolderText(item);
+                      setIsOpen(false);
+                    }}
+                  >
+                    {item}
+                  </div>
+                );
+              })}
+          </div>
         </div>
-    )
+        <div
+          className="flex-center"
+          style={{ height: "100%", padding: "8px", cursor: "pointer" }}
+        >
+          <GraphViewIcon />
+        </div>
+      </div>
+
+      <div
+        style={{
+          width: "100%",
+          border: "solid 0.5px #aaaa",
+        }}
+      />
+    </div>
+    // <div>
+    //   <Navbar color="faded" light>
+    //     <NavbarBrand onClick={HomeBand} className="mr-auto">
+    //       <label>Handbrake System</label>
+    //     </NavbarBrand>
+    //     <NavbarToggler onClick={toggleNavbar} className="mr-2" />
+    //     <Collapse isOpen={!collapsed} navbar>
+    //       <Nav navbar>
+    //         <NavItem
+    //           style={{
+    //             position: "absolute",
+    //             width: "10vw",
+    //             height: "100vh",
+    //             backgroundColor: " red",
+    //           }}
+    //         >
+    //           <NavLink onClick={HomeClick}>
+    //             <label>Home page</label>
+    //           </NavLink>
+    //         </NavItem>
+    //         <NavItem>
+    //           <NavLink onClick={AlertDataPage}>
+    //             <label>Alert page</label>
+    //           </NavLink>
+    //         </NavItem>
+    //       </Nav>
+    //     </Collapse>
+    //   </Navbar>
+    // </div>
+  );
 }
 
 export default NavBar;
