ef835f8aa90e928f47c8af8516a7d97453830275
Jack Kwok <kuenyuikwok1106@outlook.com>
2021-07-19 18:45:59 +0800

    seed modified and package.json removed duplicate modules

--- server/seeds/alertData.ts
+++ server/seeds/alertData.ts
@@ -33,20 +33,20 @@ export async function seed(knex: Knex): Promise<void> {
         {device_name: "ramp_meter_005", device_eui: "RzrIaAAqADg="}
     ]);
     await knex("alert_data").insert([
-        { device_id: 1, date: new Date("2020-06-03").toLocaleString('en-CA'), time: "05:19:53", geolocation: "22.123456,114.12346", battery: "1.95", data: "MTsyZTtjMDszNjdiMjsxMTZkYTE7MDsxYTsw"},
-        { device_id: 1, date: new Date("2020-06-04").toLocaleString('en-CA'), time: "10:25:23", geolocation: "22.432875,114.69823", battery: "1.94", data: "MTsyZTtjMDszNjdiMjsxMTZkYTE7MDsxYTsw"},
-        { device_id: 1, date: new Date("2020-06-14").toLocaleString('en-CA'), time: "16:12:30", geolocation: "22.638301,114.37821", battery: "1.93", data: "MTsyZTtjMDszNjdiMjsxMTZkYTE7MDsxYTsw"},
-        { device_id: 2, date: new Date("2020-06-15").toLocaleString('en-CA'), time: "18:12:30", geolocation: "22.638301,114.37821", battery: "1.93", data: "MTsyZTtjMDszNjdiMjsxMTZkYTE7MDsxYTsw"},
-        { device_id: 2, date: new Date("2020-06-18").toLocaleString('en-CA'), time: "19:12:30", geolocation: "22.789324,114.19024", battery: "1.71", data: "MTsyZTtjMDszNjdiMjsxMTZkYTE7MDsxYTsw"},
-        { device_id: 2, date: new Date("2020-06-23").toLocaleString('en-CA'), time: "20:12:30", geolocation: "22.784757,114.74598", battery: "1.83", data: "MTsyZTtjMDszNjdiMjsxMTZkYTE7MDsxYTsw"},
-        { device_id: 2, date: new Date("2020-06-23").toLocaleString('en-CA'), time: "18:12:35", geolocation: "22.784757,114.74598", battery: "1.82", data: "MTsyZTtjMDszNjdiMjsxMTZkYTE7MDsxYTsw"},
-        { device_id: 2, date: new Date("2020-06-23").toLocaleString('en-CA'), time: "20:12:40", geolocation: "22.784757,114.74598", battery: "1.84", data: "MTsyZTtjMDszNjdiMjsxMTZkYTE7MDsxYTsw"},
-        { device_id: 2, date: new Date("2020-06-27").toLocaleString('en-CA'), time: "21:12:30", geolocation: "22.902848,114.84925", battery: "1.91", data: "MTsyZTtjMDszNjdiMjsxMTZkYTE7MDsxYTsw"},
-        { device_id: 3, date: new Date("2020-06-24").toLocaleString('en-CA'), time: "18:12:30", geolocation: "22.819273,114.87994", battery: "1.89", data: "MTsyZTtjMDszNjdiMjsxMTZkYTE7MDsxYTsw"},
-        { device_id: 4, date: new Date("2020-06-28").toLocaleString('en-CA'), time: "13:52:30", geolocation: "22.839247,114.67521", battery: "1.62", data: "MTsyZTtjMDszNjdiMjsxMTZkYTE7MDsxYTsw"},
-        { device_id: 5, date: new Date("2020-06-29").toLocaleString('en-CA'), time: "14:22:30", geolocation: "22.901823,114.89127", battery: "1.52", data: "MTsyZTtjMDszNjdiMjsxMTZkYTE7MDsxYTsw"},
-        { device_id: 5, date: new Date("2020-06-20").toLocaleString('en-CA'), time: "15:13:30", geolocation: "22.093749,114.09123", battery: "1.73", data: "MTsyZTtjMDszNjdiMjsxMTZkYTE7MDsxYTsw"},
-        { device_id: 4, date: new Date("2020-07-04").toLocaleString('en-CA'), time: "20:15:30", geolocation: "22.817236,114.22712", battery: "1.65", data: "MTsyZTtjMDszNjdiMjsxMTZkYTE7MDsxYTsw"}
+        { device_id: 1, date: new Date("2020-06-03").toLocaleDateString('en-CA'), time: "05:19:53", geolocation: "22.123456,114.12346", battery: "1.95", data: "MTsyZTtjMDszNjdiMjsxMTZkYTE7MDsxYTsw"},
+        { device_id: 1, date: new Date("2020-06-04").toLocaleDateString('en-CA'), time: "10:25:23", geolocation: "22.432875,114.69823", battery: "1.94", data: "MTsyZTtjMDszNjdiMjsxMTZkYTE7MDsxYTsw"},
+        { device_id: 1, date: new Date("2020-06-14").toLocaleDateString('en-CA'), time: "16:12:30", geolocation: "22.638301,114.37821", battery: "1.93", data: "MTsyZTtjMDszNjdiMjsxMTZkYTE7MDsxYTsw"},
+        { device_id: 2, date: new Date("2020-06-15").toLocaleDateString('en-CA'), time: "18:12:30", geolocation: "22.638301,114.37821", battery: "1.93", data: "MTsyZTtjMDszNjdiMjsxMTZkYTE7MDsxYTsw"},
+        { device_id: 2, date: new Date("2020-06-18").toLocaleDateString('en-CA'), time: "19:12:30", geolocation: "22.789324,114.19024", battery: "1.71", data: "MTsyZTtjMDszNjdiMjsxMTZkYTE7MDsxYTsw"},
+        { device_id: 2, date: new Date("2020-06-23").toLocaleDateString('en-CA'), time: "20:12:30", geolocation: "22.784757,114.74598", battery: "1.83", data: "MTsyZTtjMDszNjdiMjsxMTZkYTE7MDsxYTsw"},
+        { device_id: 2, date: new Date("2020-06-23").toLocaleDateString('en-CA'), time: "18:12:35", geolocation: "22.784757,114.74598", battery: "1.82", data: "MTsyZTtjMDszNjdiMjsxMTZkYTE7MDsxYTsw"},
+        { device_id: 2, date: new Date("2020-06-23").toLocaleDateString('en-CA'), time: "20:12:40", geolocation: "22.784757,114.74598", battery: "1.84", data: "MTsyZTtjMDszNjdiMjsxMTZkYTE7MDsxYTsw"},
+        { device_id: 2, date: new Date("2020-06-27").toLocaleDateString('en-CA'), time: "21:12:30", geolocation: "22.902848,114.84925", battery: "1.91", data: "MTsyZTtjMDszNjdiMjsxMTZkYTE7MDsxYTsw"},
+        { device_id: 3, date: new Date("2020-06-24").toLocaleDateString('en-CA'), time: "18:12:30", geolocation: "22.819273,114.87994", battery: "1.89", data: "MTsyZTtjMDszNjdiMjsxMTZkYTE7MDsxYTsw"},
+        { device_id: 4, date: new Date("2020-06-28").toLocaleDateString('en-CA'), time: "13:52:30", geolocation: "22.839247,114.67521", battery: "1.62", data: "MTsyZTtjMDszNjdiMjsxMTZkYTE7MDsxYTsw"},
+        { device_id: 5, date: new Date("2020-06-29").toLocaleDateString('en-CA'), time: "14:22:30", geolocation: "22.901823,114.89127", battery: "1.52", data: "MTsyZTtjMDszNjdiMjsxMTZkYTE7MDsxYTsw"},
+        { device_id: 5, date: new Date("2020-06-20").toLocaleDateString('en-CA'), time: "15:13:30", geolocation: "22.093749,114.09123", battery: "1.73", data: "MTsyZTtjMDszNjdiMjsxMTZkYTE7MDsxYTsw"},
+        { device_id: 4, date: new Date("2020-07-04").toLocaleDateString('en-CA'), time: "20:15:30", geolocation: "22.817236,114.22712", battery: "1.65", data: "MTsyZTtjMDszNjdiMjsxMTZkYTE7MDsxYTsw"}
     ]);
     await knex("company_vehicles").insert([
         {company_id: 1, vehicle_id: 1 },
