ea3a959920aa0e0007c5ec7c22983acc49901a23
Shinji Hirota <shinjishinji1129@gmail.com>
2021-07-21 14:14:43 +0800

    added addNewDevice system

--- client/src/pages/ManageUser.tsx
+++ client/src/pages/ManageUser.tsx
@@ -9,15 +9,18 @@ import {
   SearchIcon,
 } from "../components/IconsOnly";
 import "../css/TablePage.css";
-import { getCompaniesDataListThunk, postCompaniesDataThunk } from "../redux/companies/thunk";
+import {
+  getCompaniesDataListThunk,
+  postCompaniesDataThunk,
+} from "../redux/companies/thunk";
 import { IRootState } from "../redux/store";
 import { manageUserTableHeaders } from "../table/tableHeader";
-import { io } from 'socket.io-client';
+import { io } from "socket.io-client";
 
 const tableHeaders = manageUserTableHeaders;
 const itemPerPage = 10;
 const TABLE_WIDTH = "75%";
-const serverUrl = process.env.REACT_APP_API_SERVER; 
+const serverUrl = process.env.REACT_APP_API_SERVER;
 
 function ManageUser() {
   const [isOpen, setIsOpen] = useState(false);
@@ -55,7 +58,7 @@ function ManageUser() {
       contactPerson: string;
       tel: string;
     }>
-  >([{companyName: "", contactPerson: "", tel: ""}]);
+  >([{ companyName: "", contactPerson: "", tel: "" }]);
 
   const handleDeleteVehicle = (idx: number) => {
     const newArr = totalVehicle.slice();
@@ -77,11 +80,18 @@ function ManageUser() {
     console.log(companyDetail);
   }, [companyDetail]);
 
-  useEffect(()=> {
+  useEffect(() => {
     const socket = io(`${serverUrl}`);
 
-    socket.on('get-new-companies', ()=>{
-      dispatch(getCompaniesDataListThunk(activePage, false, placeHolderText, searchInput));
+    socket.on("get-new-companies", () => {
+      dispatch(
+        getCompaniesDataListThunk(
+          activePage,
+          false,
+          placeHolderText,
+          searchInput
+        )
+      );
     });
 
     return () => {
@@ -144,13 +154,24 @@ function ManageUser() {
                 onChange={(e) => {
                   setSearchInput(e.target.value);
                 }}
+                onKeyDown={(e) => {
+                  if (e.key === "Enter") {
+                    dispatch(
+                      getCompaniesDataListThunk(
+                        1,
+                        true,
+                        placeHolderText,
+                        searchInput
+                      )
+                    );
+                  }
+                }}
               />
               <div
                 style={{ cursor: "pointer", padding: "8px" }}
                 onClick={
                   placeHolderText !== "Select"
                     ? () => {
-                        // dispatch() something use value: searchInput & tableHeaders[0]
                         dispatch(
                           getCompaniesDataListThunk(
                             1,
@@ -273,12 +294,13 @@ function ManageUser() {
                   <div className="flex-center formRow">
                     <div className="formLeftColumn">Company Name :</div>
                     <div className="formRightColumn">
-                      <input className="formInput" 
+                      <input
+                        className="formInput"
                         value={companyDetail[0].companyName}
-                        onChange={(e)=>{
+                        onChange={(e) => {
                           const newArr = companyDetail.slice();
                           newArr[0] = {
-                            ...companyDetail[0], 
+                            ...companyDetail[0],
                             companyName: e.target.value,
                           };
                           setCompanyDetail(newArr);
@@ -289,12 +311,13 @@ function ManageUser() {
                   <div className="flex-center formRow">
                     <div className="formLeftColumn">Contact Person :</div>
                     <div className="formRightColumn">
-                      <input className="formInput" 
+                      <input
+                        className="formInput"
                         value={companyDetail[0].contactPerson}
                         onChange={(e) => {
                           const newArr = companyDetail.slice();
                           newArr[0] = {
-                            ...companyDetail[0], 
+                            ...companyDetail[0],
                             contactPerson: e.target.value,
                           };
                           setCompanyDetail(newArr);
@@ -305,7 +328,8 @@ function ManageUser() {
                   <div className="flex-center formRow">
                     <div className="formLeftColumn">Phone Number :</div>
                     <div className="formRightColumn">
-                      <input className="formInput" 
+                      <input
+                        className="formInput"
                         value={companyDetail[0].tel}
                         onChange={(e) => {
                           const newArr = companyDetail.slice();
@@ -412,11 +436,14 @@ function ManageUser() {
                 <div className="button" onClick={() => setPopUpIsActive(false)}>
                   Cancel
                 </div>
-                <div className="button"
-                onClick={()=>{
-                  dispatch(postCompaniesDataThunk(totalVehicle, companyDetail));
-                  setPopUpIsActive(false);
-                }}
+                <div
+                  className="button"
+                  onClick={() => {
+                    dispatch(
+                      postCompaniesDataThunk(totalVehicle, companyDetail)
+                    );
+                    setPopUpIsActive(false);
+                  }}
                 >
                   Confirm
                 </div>
---
+++ client/src/pages/mockUpData.ts
@@ -0,0 +1,26 @@
+export const mockNewDevices = [
+  "11223344",
+  "!12131341234",
+  "!23123213",
+  "awfef3113r",
+  "awefawr4ty5",
+  "a4y67userg",
+];
+export const mockAllDevices = [
+  "a4tqy34",
+  "a43tq34btw4h ",
+  "a23rvq23t q3t ",
+  "a4ta 23racw3rqv34tvq",
+  "3RCQ23T AWRVA3RC",
+  "W45YW45VYTA23VQ",
+  "UIRN67UEBN5TB34T",
+  "R67UNE45YBW34TV3",
+  "E456UBW45VSERGFB45YNW",
+  "AW3R5YNE5YB3TW4B",
+  "34TV46UNW34V3R4",
+  "A34TVW45YBSERFVA",
+  "34TVE46UYBSERVFAWEAV",
+  "5YBE465YBWSERFV34TB",
+  "4TBW45YWVEFAB34TB",
+  "A34TBW34TBW34TVQ34",
+];
