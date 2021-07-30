ec087dee180fc6d3941f4b8d165ffb160aef8739
muselabs-ym <yeeman.ma@muselabs-eng.com>
2021-07-30 17:37:34 +0800

    removed unused api routes

--- server/routes/dataRoute.ts
+++ server/routes/dataRoute.ts
@@ -9,34 +9,18 @@ dataRoutes.put(`/alertData`, dataController.putAlertData);
 dataRoutes.delete(`/alertData`, dataController.deleteAlertData);
 // update later
 dataRoutes.get(`/history`, dataController.getHistoryData);
-dataRoutes.post(`/history`);
-dataRoutes.put(`/history`);
-dataRoutes.delete(`/history`);
 
 dataRoutes.get(`/companies`, dataController.getCompaniesData);
 dataRoutes.post(`/companies`, dataController.postCompaniesData);
-dataRoutes.put(`/companies`);
-dataRoutes.delete(`/companies`);
 
-dataRoutes.get(`/vehicles`);
 dataRoutes.post(`/vehicles/:id`, dataController.postVehicles);
-dataRoutes.put(`/vehicles`);
-dataRoutes.delete(`/vehicles`);
 
 dataRoutes.get(`/devices`, dataController.getDevicesData);
-dataRoutes.post(`/devices`);
-dataRoutes.put(`/devices`);
-dataRoutes.delete(`/devices`);
 
-dataRoutes.get(`/vehicle_device`);
 dataRoutes.post(`/vehicle_device`, dataController.postVehicleDevice); // profile add new link
 dataRoutes.put(`/vehicle_device`); // profile update exist vehicle_device
-dataRoutes.delete(`/vehicle_device`);
 
 dataRoutes.get(`/devices/version`, dataController.getDevicesVersion);
-dataRoutes.post(`/devices/version`);
-dataRoutes.put(`/devices/version`);
-dataRoutes.delete(`/devices/version`);
 
 // dataRoutes.get(`/profile`);
 dataRoutes.get(`/profile/:id`, dataController.getProfileData);
