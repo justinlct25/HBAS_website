db15d202df10dab49f70236558d46ede6a29fcb3
Shinji Hirota <shinjishinji1129@gmail.com>
2021-08-03 11:28:23 +0800

    use google map

--- client/src/pages/IncidentPage.tsx
+++ client/src/pages/IncidentPage.tsx
@@ -1,17 +1,28 @@
 import React, { useState } from "react";
-import ReactMapboxGl, { Feature, Layer } from "react-mapbox-gl";
-import 'mapbox-gl/dist/mapbox-gl.css';
+import GoogleMapReact from "google-map-react";
 import { useSelector } from "react-redux";
 import { useHistory } from "react-router-dom";
 import { BackButton, CaretIcon } from "../components/IconsOnly";
 import "../css/TablePage.css";
 import { IRootState } from "../redux/store";
 
-const MAPBOX_TOKEN =
-  "pk.eyJ1Ijoic2hpbmppMTEyOSIsImEiOiJja3F5eGcwYzMwYXlwMnVtbjhyOTl2OGI1In0.6BzqsFZ2JUSv2QkMWJy-ng";
-const Map = ReactMapboxGl({
-  accessToken: MAPBOX_TOKEN,
-});
+interface IncidentPointProps {
+  lat: number;
+  lng: number;
+}
+const IncidentPoint = (props: IncidentPointProps) => {
+  const { lat, lng } = props;
+  return (
+    <div
+      style={{
+        width: "25px",
+        height: "25px",
+        background: "#0FF",
+        borderRadius: "50%",
+      }}
+    />
+  );
+};
 
 function IncidentPage() {
   const history = useHistory();
@@ -66,64 +77,24 @@ function IncidentPage() {
             width: "100%",
           }}
         >
-          <div className="flex-center satelliteViewButtonContainer">
-            <div
-              className="flex-center satelliteButton"
-              style={{
-                borderRadius: "8px 0px 0px 8px",
-                color: isLiveView ? "#EEE" : "#555",
-                background: isLiveView ? "rgba(94, 147, 220, 0.76)" : "#FFF",
-              }}
-              onClick={() => setIsLiveView(true)}
-            >
-              {"Satellite View"}
-            </div>
-            <div
-              className="flex-center satelliteButton"
-              style={{
-                borderRadius: "0px 8px 8px 0px",
-                color: isLiveView ? "#555" : "#EEE",
-                background: isLiveView ? "#FFF" : "rgba(94, 147, 220, 0.76)",
-              }}
-              onClick={() => setIsLiveView(false)}
-            >
-              {"Map View"}
-            </div>
-          </div>
           {/* Map here */}
-          {/* <div>{incidentPageData.longitude}</div>
-          <div>{incidentPageData.latitude}</div> */}
-          <Map
-            style={
-              isLiveView
-                ? "mapbox://styles/shinji1129/ckr4cxoe30c9i17muitq9vqvo"
-                : "mapbox://styles/shinji1129/ckr4d9iy60ci317mte2mzob6k"
-            }
-            // containerStyle={{
-            //   height: "100%",
-            //   width: "100%",
-            // }}
-            // zoom={[12]}
-            // center={[incidentLocation.longitude, incidentLocation.latitude]}
-            // onStyleLoad={() =>
-            //   setIncidentLocation({
-            //     longitude: incidentPageData.longitude,
-            //     latitude: incidentPageData.latitude,
-            //   })
-            // }
+
+          <GoogleMapReact
+            bootstrapURLKeys={{
+              key: process.env.REACT_APP_API_GOOGLE_MAP as string,
+            }}
+            defaultCenter={{
+              lat: incidentLocation.latitude,
+              lng: incidentLocation.longitude,
+            }}
+            defaultZoom={18}
           >
-            {/* <Layer
-              type="circle"
-              paint={{ "circle-color": "#00FFFF", "circle-radius": 10 }}
-            >
-              <Feature
-                coordinates={[
-                  incidentLocation.longitude,
-                  incidentLocation.latitude,
-                ]}
-              />
-            </Layer> */}
-          </Map>
+            <IncidentPoint
+              lat={incidentLocation.latitude}
+              lng={incidentLocation.longitude}
+            />
+          </GoogleMapReact>
+
           <div
             className={
               isReportOpen
