2d20c7034c3d4b7030208436c7b38ad589ec30bc
Alan Lee <abc2534yhj@gmail.com>
2021-08-04 16:42:52 +0800

    update real devices & vehicles data in seed

--- server/seeds/seedFor20210804.ts
+++ server/seeds/seedFor20210804.ts
@@ -1,9 +1,21 @@
 import { Knex } from "knex";
-import { insertDevices, insertData } from "./20210804-rawDataset";
+import { insertDevices, insertData, insertCars, insertVehicleDevice } from "../utils/20210804-rawDataset";
 
+type InsertVehicles = { id: number; car_plate: string };
 type InsertDevices = { id: number; device_eui: string };
 
 export async function seed(knex: Knex): Promise<void> {
+
+  // vehicles
+  const vehicles = await knex<InsertVehicles>('vehicles')
+    .insert(insertCars)
+    .returning(['car_plate', 'id']);
+
+  const vehiclesMap = vehicles.reduce((mapping, vehicle) => {
+    mapping.set(vehicle.car_plate, vehicle.id);
+    return mapping;
+  }, new Map<string, number>());
+
     // devices
   const devices = await knex<InsertDevices>('devices')
   .insert(insertDevices)
@@ -26,4 +38,13 @@ export async function seed(knex: Knex): Promise<void> {
   }));
 
   await knex('alert_data').insert(alertData);
+
+  // const vehicle-device
+  const vehicleDevice = insertVehicleDevice.map((session) => ({
+    device_id: devicesMap.get(session.device_eui),
+    vehicle_id: vehiclesMap.get(session.car_plate),
+  }));
+
+  await knex('vehicle_device').insert(vehicleDevice);
+  
 }
