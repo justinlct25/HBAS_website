204dc0ff77f7df35bbc89def793fcbfe75ad42c2
muselabs-ym <yeeman.ma@muselabs-eng.com>
2021-08-06 18:49:22 +0800

    amended io messages

--- client/src/pages/PulseMessagePage.tsx
+++ client/src/pages/PulseMessagePage.tsx
@@ -40,7 +40,7 @@ function PulseMessagePage() {
   useEffect(() => {
     const socket = io(`${REACT_APP_API_SERVER}`);
 
-    socket.on("get-new-alertData", () => {
+    socket.on("new-data-type-B", () => {
       dispatch(getAlertDataListThunk(activePage, false));
     });
 
