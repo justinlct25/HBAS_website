d4408dd38df0de00624f2d2c6032f5a9bc8e146a
Shinji Hirota <shinjishinji1129@gmail.com>
2021-08-26 16:33:27 +0800

    temporary changes for demo purpose

--- server/services/AlertDataService.ts
+++ server/services/AlertDataService.ts
@@ -116,31 +116,24 @@ export class AlertDataService {
         companyName: `${tables.COMPANIES}.company_name`,
       })
       .innerJoin(tables.DEVICES, `${tables.ALERT_DATA}.device_id`, `${tables.DEVICES}.id`)
-      .innerJoin(
-        tables.VEHICLE_DEVICE,
-        `${tables.DEVICES}.id`,
-        `${tables.VEHICLE_DEVICE}.device_id`
-      )
-      .innerJoin(tables.VEHICLES, `${tables.VEHICLES}.id`, `${tables.VEHICLE_DEVICE}.vehicle_id`)
-      .innerJoin(
+      .leftJoin(tables.VEHICLE_DEVICE, `${tables.DEVICES}.id`, `${tables.VEHICLE_DEVICE}.device_id`)
+      .leftJoin(tables.VEHICLES, `${tables.VEHICLES}.id`, `${tables.VEHICLE_DEVICE}.vehicle_id`)
+      .leftJoin(
         tables.COMPANY_VEHICLES,
         `${tables.COMPANY_VEHICLES}.vehicle_id`,
         `${tables.VEHICLES}.id`
       )
-      .innerJoin(
-        tables.COMPANIES,
-        `${tables.COMPANY_VEHICLES}.company_id`,
-        `${tables.COMPANIES}.id`
-      )
+      .leftJoin(tables.COMPANIES, `${tables.COMPANY_VEHICLES}.company_id`, `${tables.COMPANIES}.id`)
       .where({
         [`${tables.ALERT_DATA}.is_active`]: true,
         [`${tables.DEVICES}.is_active`]: true,
-        [`${tables.VEHICLE_DEVICE}.is_active`]: true,
-        [`${tables.VEHICLES}.is_active`]: true,
-        [`${tables.COMPANY_VEHICLES}.is_active`]: true,
-        [`${tables.COMPANIES}.is_active`]: true,
+        // [`${tables.VEHICLE_DEVICE}.is_active`]: true,
+        // [`${tables.VEHICLES}.is_active`]: true,
+        // [`${tables.COMPANY_VEHICLES}.is_active`]: true,
+        // [`${tables.COMPANIES}.is_active`]: true,
       })
       .andWhereBetween(`${tables.ALERT_DATA}.date`, [d, new Date()])
+      .andWhereNot(`${tables.ALERT_DATA}.address`, 'GPS NOT FOUND')
       .orderBy([
         { column: `${tables.ALERT_DATA}.device_id`, order: 'asc' },
         { column: `${tables.ALERT_DATA}.date`, order: 'desc' },
