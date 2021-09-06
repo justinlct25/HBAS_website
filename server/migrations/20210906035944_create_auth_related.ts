c0a51ab90a949596da45304f839a32d6c95e2510
muselabs-ym <yeeman.ma@muselabs-eng.com>
2021-09-06 12:20:19 +0800

    HAS-49 added migration file to add auth-related tables

---
+++ server/migrations/20210906035944_create_auth_related.ts
@@ -0,0 +1,31 @@
+import { Knex } from 'knex';
+
+const usersTable = 'users';
+const devicesTable = 'devices';
+const userDevices = 'user_devices';
+
+export async function up(knex: Knex): Promise<void> {
+  await knex.schema.createTable(usersTable, (table) => {
+    table.increments();
+    table.string('email').notNullable();
+    table.string('password').notNullable();
+    table.string('role').notNullable().defaultTo('USER');
+    table.boolean('is_active').notNullable().defaultTo(true);
+    table.timestamps(false, true);
+  });
+
+  await knex.schema.createTable(userDevices, (table) => {
+    table.increments();
+    table.integer('user_id').notNullable().unsigned();
+    table.foreign('user_id').references(`${usersTable}.id`);
+    table.integer('device_id').notNullable().unsigned();
+    table.foreign('device_id').references(`${devicesTable}.id`);
+    table.boolean('is_active').notNullable().defaultTo(true);
+    table.timestamps(false, true);
+  });
+}
+
+export async function down(knex: Knex): Promise<void> {
+  await knex.schema.dropTable(userDevices);
+  await knex.schema.dropTable(usersTable);
+}
