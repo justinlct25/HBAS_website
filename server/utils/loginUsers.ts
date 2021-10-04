24ff11df2493f34d34017bb6c2260dea9b379527
muselabs-ym <yeeman.ma@muselabs-eng.com>
2021-10-04 11:52:45 +0800

    added new users for temp login

--- server/utils/loginUsers.ts
+++ server/utils/loginUsers.ts
@@ -2,4 +2,7 @@ export const authorizedUser = Object.freeze({
   admin1: { username: 'admin1', password: 'password1' },
   admin2: { username: 'admin2', password: 'password2' },
   admin3: { username: 'admin3', password: 'password3' },
+  kin: { username: 'kin', password: '426327253' },
+  CEDD1: { username: 'CEDD1', password: '426327253' },
+  CEDD2: { username: 'CEDD2', password: '426327253' },
 });
