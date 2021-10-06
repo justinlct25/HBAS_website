2cc21d5c8777544b91c419f3b2b436e68b78ed91
Shinji Hirota <shinjishinji1129@gmail.com>
2021-10-06 11:21:34 +0800

    Fix company name detection

--- client/src/pages/LoginPage.tsx
+++ client/src/pages/LoginPage.tsx
@@ -5,12 +5,15 @@ import { useDispatch, useSelector } from "react-redux";
 import { login } from "../redux/login/thunk";
 import { IRootState } from "../redux/store";
 import styles from "./LoginPage.module.scss";
+import { setIsLoadingAction } from "../redux/loading/action";
+import Loading from "../components/Loading";
 
 function LoginPage() {
+  const [loginInput, setLoginInput] = useState({ username: "", password: "" });
+  const [companyName, setCompanyName] = useState("BEE");
   const dispatch = useDispatch();
   const isLoggedIn = useSelector((state: IRootState) => state.login.isLoggedIn);
-
-  const [loginInput, setLoginInput] = useState({ username: "", password: "" });
+  const isLoading = useSelector((state: IRootState) => state.loading.loading.isLoading);
 
   const handleSubmit = () => {
     dispatch(login(loginInput.username, loginInput.password));
@@ -22,40 +25,61 @@ function LoginPage() {
     }
   }, [dispatch, isLoggedIn]);
 
+  useEffect(() => {
+    dispatch(setIsLoadingAction(true));
+    const pathname = window.location.hostname.split(".");
+    if (pathname[0] === "handbrake") {
+      setCompanyName("Muse Labs");
+    } else {
+      setCompanyName("BEE");
+    }
+    dispatch(setIsLoadingAction(false));
+  }, []);
+
   return (
     <div className={`${styles.container}${isMobile ? " " + styles.mobile : ""}`}>
-      <div>
-        {/* Please use semantic tags like <h1> instead...
+      {isLoading ? (
+        <div>
+          <Loading />
+        </div>
+      ) : (
+        <div>
+          {/* Please use semantic tags like <h1> instead...
         And fix the css on TablePage.css so it doesn't override <h1> globally... */}
-        <div className="titleText">Muse Labs - Handbrake Alert System</div>
-        <div className="titleText">Admin Login</div>
-        <form onSubmit={e => {
-          e.preventDefault();
-          handleSubmit();
-        }}>
-          <label>Username :</label>
-          <input
-            value={loginInput.username}
-            onChange={(e) => {
-              setLoginInput({
-                username: e.target.value,
-                password: loginInput.password,
-              });
+          <div className="titleText">{`${companyName} - Handbrake Alert System`}</div>
+          <div className="titleText">Admin Login</div>
+          <form
+            onSubmit={(e) => {
+              e.preventDefault();
+              handleSubmit();
             }}
-          />
-          <label>Password :</label>
-          <input
-            type="password"
-            onChange={(e) => {
-              setLoginInput({
-                username: loginInput.username,
-                password: e.target.value,
-              });
-            }}
-          />
-          <button className="button" type="submit">Login</button>
-        </form>
-      </div>
+          >
+            <label>Username :</label>
+            <input
+              value={loginInput.username}
+              onChange={(e) => {
+                setLoginInput({
+                  username: e.target.value,
+                  password: loginInput.password,
+                });
+              }}
+            />
+            <label>Password :</label>
+            <input
+              type="password"
+              onChange={(e) => {
+                setLoginInput({
+                  username: loginInput.username,
+                  password: e.target.value,
+                });
+              }}
+            />
+            <button className="button" type="submit">
+              Login
+            </button>
+          </form>
+        </div>
+      )}
     </div>
   );
 }
