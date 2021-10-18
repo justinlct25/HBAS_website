a56638a2ccd9a9c9ba839693c49615240a752c30
muselabs-ym <yeeman.ma@muselabs-eng.com>
2021-10-18 16:04:58 +0800

    added comment for routes

--- server/routes/devices.routes.ts
+++ server/routes/devices.routes.ts
@@ -5,6 +5,9 @@ import { createAsyncMiddleware as CAM } from '../utils/middleware';
 export const devicesUserRoutes = express.Router();
 export const devicesAdminRoutes = express.Router();
 
+/* -------------------- users routes -------------------- */
 devicesUserRoutes.get('/', CAM(devicesController.getAllDevices));
+
+/* -------------------- admin routes -------------------- */
 devicesAdminRoutes.get('/link-device-vehicle', CAM(devicesController.getDevicesForLinking));
 devicesAdminRoutes.post('/link-device-vehicle', CAM(devicesController.linkDeviceAndVehicle));
