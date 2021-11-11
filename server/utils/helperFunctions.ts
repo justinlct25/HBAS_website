393b328276f2cefd7b2be99015213186c0324693
muselabs-ym <yeeman.ma@muselabs-eng.com>
2021-11-11 14:21:55 +0800

    use hkt timezone for date query

--- server/utils/helperFunctions.ts
+++ server/utils/helperFunctions.ts
@@ -1,9 +1,9 @@
 export const setTimeToDateStart = (date: Date) => {
   date.setHours(0, 0, 0, 0);
-  return date.toISOString();
+  return date.toISOString().replace('Z', '+08:00');
 };
 
 export const setTimeToDateEnd = (date: Date) => {
   date.setHours(23, 59, 59, 999);
-  return date.toISOString();
+  return date.toISOString().replace('Z', '+08:00');
 };
